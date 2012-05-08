5eb17cb0efb2a4b6f27aadc67f41a018cba07559
liquidemo <liquidemo2012@gmail.com>
2012-05-08 11:08:41 +0200

    updated menagerie examples

---
+++ menagerie/backup/restore.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo 
+echo "*** WARNING *********************************************"
+echo "This script will drop the PROD and STAGE database as well"
+echo "as the liquibase user!                                   "
+echo "                                                         "
+echo "Enter mysql root password to continue or CTRL-C to abort."
+echo "*********************************************************"
+echo 
+
+DIR="$(cd $(dirname $0) && pwd)"
+
+mysql -uroot -p <<EOF
+    DROP DATABASE IF EXISTS stage;
+    DROP DATABASE IF EXISTS prod;
+    GRANT USAGE ON *.* TO 'liquibase'@'localhost';
+    DROP USER 'liquibase'@'localhost';
+    CREATE USER 'liquibase'@'localhost' IDENTIFIED BY 'liquibase';
+    CREATE DATABASE stage;
+    CREATE DATABASE prod;
+    GRANT ALL ON stage.* TO 'liquibase'@'localhost';
+    GRANT ALL ON prod.* TO 'liquibase'@'localhost';
+EOF
+
+mysql -uliquibase -pliquibase prod < $DIR/menagerie.sql
+
+rm -rf $DIR/db
+mkdir -p $DIR/db
+cp $DIR/test.h2.db $DIR/../db
